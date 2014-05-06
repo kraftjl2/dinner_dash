ada6f76f4242f69f87471571d441cd69805920ea
kraftjl2 <kraftjl2@miamioh.edu>
2014-05-06 13:15:33 +0000

    added page titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  
+  def active_class (url = '')
+    'active' if current_page?(url)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ DinnerDash::Application.routes.draw do
   
   resources :categories
   resources :items
+  
+  get 'about', to: 'marketing#about'
+  get 'faqs', to: 'marketing#faqs'
 
-  root 'application#index'
+  root 'marketing#index'
 
   
 end
