ad49cb0618b756889b62ac4c84893107c4862bda
kraftjl2 <kraftjl2@miamioh.edu>
2014-05-06 16:24:00 +0000

    Added Menu Page and partials to show categories only with items

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ DinnerDash::Application.routes.draw do
   
   resources :categories
   resources :items
+  resources :menu, only: [:index]
   
   get 'about', to: 'marketing#about'
   get 'faqs', to: 'marketing#faqs'
 
   root 'marketing#index'
-
   
 end
